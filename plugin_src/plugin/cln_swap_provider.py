e7c3c972393a8c85e852fe6aa72e85fb9d98b49c
fbock <fbock@student.42heilbronn.de>
2024-10-30 10:10:12 +0100

    add is_initialized method to CLNSwapProvider and allow for dep inj

--- plugin_src/plugin/cln_swap_provider.py
+++ plugin_src/plugin/cln_swap_provider.py
@@ -4,25 +4,30 @@ from .cln_logger import PluginLogger
 from .cln_plugin import CLNPlugin
 from .cln_chain import CLNChainWallet
 from .cln_lightning import CLNLightning
-from .globals import plugin_logger
 from .plugin_config import PluginConfig
 from .cln_storage import CLNStorage
 from .json_db import JsonDB
 from .submarine_swaps import SwapManager
 
 
-# from plugin.submarine_swaps import SwapManager
-
 class CLNSwapProvider:
-    def __init__(self):
-        self.plugin_handler: Optional[CLNPlugin] = None
-        self.logger: Optional[PluginLogger] = None
-        self.config: Optional[PluginConfig] = None
-        self.json_db: Optional[JsonDB] = None
-        self.cln_chain_wallet: Optional[CLNChainWallet] = None
-        self.cln_lightning: Optional[CLNLightning] = None
-        self.swap_manager: Optional[SwapManager] = None
-        self.is_initialized: bool = False
+    def __init__(
+        self,
+        plugin_handler: Optional[CLNPlugin] = None,
+        logger: Optional[PluginLogger] = None,
+        config: Optional[PluginConfig] = None,
+        json_db: Optional[JsonDB] = None,
+        cln_chain_wallet: Optional[CLNChainWallet] = None,
+        cln_lightning: Optional[CLNLightning] = None,
+        swap_manager: Optional[SwapManager] = None
+    ):
+        self.plugin_handler = plugin_handler
+        self.logger = logger
+        self.config = config
+        self.json_db = json_db
+        self.cln_chain_wallet = cln_chain_wallet
+        self.cln_lightning = cln_lightning
+        self.swap_manager = swap_manager
 
     async def initialize(self):
         # cln plugin handler
@@ -58,11 +63,21 @@ class CLNSwapProvider:
                                         plugin_config=self.config,
                                         logger=self.logger)
 
-        self.is_initialized = True
-
     async def run(self):
         if not self.is_initialized:
             await self.initialize()
         await self.swap_manager.main_loop()
         raise Exception("CLNSwapProvider main loop exited unexpectedly")
+
+    @property
+    def is_initialized(self) -> bool:
+        if (self.plugin_handler
+            and self.logger
+            and self.config
+            and self.json_db
+            and self.cln_chain_wallet
+            and self.cln_lightning
+            and self.swap_manager):
+            return True
+        return False
 
