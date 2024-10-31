415dad8bf89369e3819f65905030c095c18b5ca3
fbock <fbock@student.42heilbronn.de>
2024-10-31 09:42:18 +0100

    fix dynamic hook handler with Event() sync

--- plugin_src/plugin/cln_plugin.py
+++ plugin_src/plugin/cln_plugin.py
@@ -4,6 +4,7 @@ from typing import Optional
 from pycparser.ply.yacc import restart
 from pyln.client.plugin import JSONType
 from pyln.client import Plugin
+from threading import Event
 import asyncio
 
 
@@ -11,12 +12,13 @@ class CLNPlugin:
     def __init__(self):
         self.plugin = Plugin()
         self.htlc_hook = None  # type: Optional[Callable[..., JSONType]]
+        self._hook_ready = Event()
         self.plugin.add_hook("htlc_accepted", self.htlc_hook_handler)
         self.thread = asyncio.to_thread(self.plugin.run)  # the plugin is blocking to read stdin so we run it in a thread
 
     def __await__(self):
         async def _check_running():
-            """Check if plugin.run() has returned in the thread"""
+            """Check if plugin.run() has returned in the thread, if so the handshake with CLN failed"""
             task = asyncio.create_task(self.thread)  # temporarily create a task to check if the plugin is running
             await asyncio.sleep(5)  # sleep some time to leave the plugin time to do CLN handshake
             if task.done():
@@ -25,14 +27,14 @@ class CLNPlugin:
         return _check_running().__await__()
 
     def htlc_hook_handler(self, onion, htlc, request, plugin, *args, ** kwargs) -> JSONType:
-        import sys
-        print(f"htlc_hook_handler {type(self.htlc_hook)}", file=sys.stderr)
-        if self.htlc_hook is None:
-           return {"result": "continue"}
+        """Dynamic htlc hook handler, calls the hook in self.htlc_hook"""
+        if not self._hook_ready.is_set():
+            return {"result": "continue"}
         return self.htlc_hook(onion, htlc, request, plugin, *args, **kwargs)
 
     def set_htlc_hook(self, hook: Callable[..., JSONType]) -> None:
         self.htlc_hook = hook
+        self._hook_ready.set()
 
     def derive_secret(self, derivation_str: str) -> bytes:
         """Derive a secret from CLN HSM secret (for use as Nostr secret)"""
@@ -43,7 +45,3 @@ class CLNPlugin:
         secret_bytes = bytes.fromhex(secret_hex)
         assert len(secret_bytes) == 32
         return secret_bytes
-
-    # def _htlc_accepted(self, payload: dict):
-    #     """Called when a new HTLC is accepted"""
-        # do something with the payload
