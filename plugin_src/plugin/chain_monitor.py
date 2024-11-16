c54e8786bfbd0acffe2c9f4af5705268f3f1f454
f321x <f321x@tutamail.com>
2024-11-16 22:15:22 +0100

    add txindex check to ChainMonitor

--- plugin_src/plugin/chain_monitor.py
+++ plugin_src/plugin/chain_monitor.py
@@ -1,4 +1,6 @@
 from typing import Callable, Optional, Dict
+
+from httpx import Timeout as HttpxTimeout
 from bitcoinrpc import BitcoinRPC
 import time
 
@@ -28,10 +30,22 @@ class ChainMonitor:
         except Exception as e:
             raise ChainMonitorNotConnectedError(f"ChainMonitor: Could not connect to Bitcoin Core: {e}")
 
+    async def _txindex_enabled(self) -> bool:
+        """Check if txindex is enabled"""
+        try:
+            result = await self.bcore.acall(method="getindexinfo", params=[], timeout=HttpxTimeout(5))
+            if not result.get("txindex", False) or not result["txindex"].get("synced", False):
+                return False
+            return True
+        except Exception as e:
+            raise ChainMonitorRpcError(f"ChainMonitor _txindex_enabled: Could not get blockchain info: {e}")
+
     async def run(self) -> None:
         """Run the chain monitor"""
         await self._test_connection()
-        pass  # TODO: Implement
+        if not await self._txindex_enabled():
+            raise ChainMonitorRpcError("ChainMonitor: txindex is not enabled")
+        pass  # TODO: Implement callback monitoring
 
     def add_callback(self, lookup_address, callback: Callable) -> None:
         self.monitored_addresses.add(lookup_address)
@@ -48,8 +62,8 @@ class ChainMonitor:
             if not result["blocks"] == result["headers"]:
                 return False
 
-            best_block_time = await self.bcore.getblock(block_hash=result["bestblockhash"],
-                                                   verbosity=1)["time"]
+            best_block_time = await self.bcore.getblockheader(block_hash=result["bestblockhash"],
+                                                   verbose=True)["time"]
             # if last block is older than 60 minutes something is probably wrong and we should wait
             if best_block_time < time.time() - 60 * 60:
                 return False
@@ -58,7 +72,19 @@ class ChainMonitor:
         return True
 
     def get_tx_height(self, txid_hex: str) -> TxMinedInfo:
-        pass
+        try:
+            height = self.bcore.getblockcount()
+            raw_tx = self.bcore.getrawtransaction(txid=txid_hex, verbose=True)
+            return TxMinedInfo(
+                height=raw_tx["confirmations"] - height if raw_tx["confirmations"] > 0 else None,
+                conf=raw_tx["confirmations"],
+                timestamp=raw_tx["blocktime"],
+                txpos=None,  # we don't have this info and don't need it
+                header_hash=raw_tx["blockhash"],
+                wanted_height=raw_tx["locktime"] if raw_tx["locktime"] > 0 else None,
+            )
+        except Exception as e:
+            raise ChainMonitorRpcError(f"ChainMonitor get_tx_height: Could not get raw transaction: {e}")
 
     def get_transaction(self, txid_hex: str) -> Optional[Transaction]:
         """getrawtransaction"""
